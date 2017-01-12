2dde8c43a74f0a7e67a99a300d73466fe6830351
Emma Blixt <emma.blixt@home.se>
2017-01-12 19:35:28 +0100

    CSS Wayfinder
    Some CSS art as a tribute to my favorite video game series.

---
+++ wayfinder.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<head>
+<title>CSS Wayfinder</title>
+<link rel="stylesheet" type="text/css" href="wayfinder.css">
+</head>
+<body>
+
+
+<div id="wayfinder-container">
+
+  <div id="center">
+    <div id="center-bottom"></div>
+    <div id="lines"></div>
+    <div id="outside-lines"></div>
+    <div id="bottom-lines"></div>
+  </div> <!--  end of #center -->
+
+  <div class="wayfinder">
+    <div class="shell">
+      <div class="top"></div>
+      <div class="rope"></div>
+    </div> <!--  end of .shell -->
+  </div> <!--  end of .wayfinder -->
+
+  <div class="wayfinder-2">
+    <div class="shell">
+      <div class="top"></div>
+      <div class="rope"></div>
+    </div> <!--  end of .shell -->
+  </div> <!--  end of .wayfinder-2 -->
+
+  <div class="wayfinder-3">
+    <div class="shell">
+      <div class="top"></div>
+      <div class="rope"></div>
+    </div> <!--  end of .shell -->
+  </div> <!--  end of .wayfinder-3 -->
+
+  <div class="wayfinder-4">
+    <div class="shell">
+      <div class="top"></div>
+      <div class="rope"></div>
+    </div> <!--  end of .shell -->
+  </div> <!--  end of .wayfinder-4 -->
+
+  <div class="wayfinder-5">
+    <div class="shell">
+      <div class="top"></div>
+      <div class="rope"></div>
+    </div> <!--  end of .shell -->
+  </div> <!--  end of .wayfinder-5 -->
+
+</div> <!--  end of #wayfinder-container -->
+
+</body>
+</html>
